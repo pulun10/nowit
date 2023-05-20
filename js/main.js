10ad78253c822ffb7c42d34123324f55ddc7237e
pulun10 <pulun98@gmail.com>
2023-05-20 17:51:24 -0300

    inicio project nowit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// nav toggle
+const btnNav = document.querySelector('.btn-nav');
+const nav = document.querySelector('nav');
+btnNav.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+})
