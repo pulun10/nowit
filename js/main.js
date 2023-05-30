649f8f5aec1cd96fc1cb95a49a90a80deeb36108
pulun10 <pulun98@gmail.com>
2023-05-30 00:05:17 -0300

    cambios de background

--- js/main.js
+++ js/main.js
@@ -1,11 +1,6 @@
-const Hola = prompt("Hola Mundo");
-
-
 // nav toggle
 const btnNav = document.querySelector('.btn-nav');
 const nav = document.querySelector('nav');
 btnNav.addEventListener('click', () => {
     nav.classList.toggle('nav-active');
-})
-
-Hola();
+});
